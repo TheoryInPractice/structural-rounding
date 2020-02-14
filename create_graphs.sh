59d6ce1d79051033123a778d5e2fd37fa52aa7c7
Brian Lavallee <bplavallee@gmail.com>
2020-02-14 07:27:20 -0700

    replaced 2 spaces with 4-space tabs in create graphs

--- create_graphs.sh
+++ create_graphs.sh
@@ -5,17 +5,17 @@ if [ ! -f generator/generator.out ]; then
 fi
 
 if [ "$1" = "small" ]; then
-  mkdir -p graphs/small
-  ./generator/create_graphs_vc.sh 4000000 graphs/small
+    mkdir -p graphs/small
+    ./generator/create_graphs_vc.sh 4000000 graphs/small
 elif [ "$1" = "medium" ]; then
-  mkdir -p graphs/medium
-  ./generator/create_graphs_vc.sh 40000000 graphs/medium
+    mkdir -p graphs/medium
+    ./generator/create_graphs_vc.sh 40000000 graphs/medium
 elif [ "$1" = "large" ]; then
-  mkdir -p graphs/large
-  ./generator/create_graphs_vc.sh 200000000 graphs/large
+    mkdir -p graphs/large
+    ./generator/create_graphs_vc.sh 200000000 graphs/large
 elif [ "$1" = "test" ]; then
-  mkdir -p graphs/test
-  ./generator/create_graphs_vc.sh 100000 graphs/test
+    mkdir -p graphs/test
+    ./generator/create_graphs_vc.sh 100000 graphs/test
 else
-  echo "Provide 'small', 'medium', or 'large' as an argument or use 'create_graphs_custom <edges> <directory>'"
+    echo "Provide 'small', 'medium', or 'large' as an argument or use 'create_graphs_custom <edges> <directory>'"
 fi
