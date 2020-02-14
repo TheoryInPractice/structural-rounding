27b080f4462cfeaa0bfc1e668db006b271f482d7
Brian Lavallee <bplavallee@gmail.com>
2020-02-14 07:32:14 -0700

    moved create graphs custom functionality into create graphs

--- create_graphs.sh
+++ create_graphs.sh
@@ -16,6 +16,8 @@ elif [ "$1" = "large" ]; then
 elif [ "$1" = "test" ]; then
     mkdir -p graphs/test
     ./generator/create_graphs_vc.sh 100000 graphs/test
+elif [ "$1" = "custom" ] && [ $# -gt 2 ]; then
+    ./generator/create_graphs_vc.sh $2 $3
 else
-    echo "Provide 'small', 'medium', or 'large' as an argument or use 'create_graphs_custom <edges> <directory>'"
+    echo "Provide 'small', 'medium', or 'large' as an argument or use 'create_graphs custom <edges> <directory>'"
 fi
